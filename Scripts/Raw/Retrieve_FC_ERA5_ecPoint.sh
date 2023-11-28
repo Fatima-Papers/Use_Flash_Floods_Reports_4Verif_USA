3c24d060bd531900a5688855c7ceb7d1f69f1594
Fatima Pillosu <fatima.pillosu@ecmwf.int>
2023-11-28 11:10:48 +0000

    Added scripts that retrieve the raw forecasts (ENS, ecPoint and ERA5_ecPoint), observations (NOAA_reports), and computes the USA domain.

---
+++ Scripts/Raw/Retrieve_FC_ERA5_ecPoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+###############################################################################
+# CODE DESCRIPTION
+# Retrieve_FC_ERA5_ecPoint retrieves the ERA5-ecPoint data for 12-hourly rainfall.  
+# Runtime: the code can take up to 31 hours to run in series.
+
+# INPUT PARAMETERS DESCRIPTION
+# YearS (date, in YYYY format): start year to retrieve.
+# YearF (date, in YYYY format): final year to retrieve.
+# Acc (integer, in hours): forecast accumulation period.                            
+# Git_Repo (string): repository's local path.
+# DirIN (string): full path containing the forecasts to retrieve.
+# DirOUT (string): relative path containing the retrieved forecasts.
+
+# INPUT PARAMETERS
+YearS=1950
+YearF=2020
+Acc=12
+Git_Repo="/ec/vol/ecpoint_dev/mofp/Papers_2_Write/Use_Flash_Floods_Reports_4Verif_USA"
+DirIN="/ec/vol/highlander/ERA5_ecPoint_70yr"
+DirOUT="Data/Raw/FC/ERA_ecPoint"
+###############################################################################
+
+# Setting general variables
+AccSTR=$(printf %02d ${Acc})
+
+# Setting output directory
+DirOUT_temp=${Git_Repo}/${DirOUT}
+mkdir -p ${DirOUT_temp}/Pt_BC_PERC
+mkdir -p ${DirOUT_temp}/WT
+
+# Retrieving the ERA5_ecPoint data
+for Year in $(seq ${YearS} ${YearF}); do
+    echo "Retrieving ERA5_ecPoint for ${Year}"
+    cp -r ${DirIN}/Rainfall_${AccSTR}h/Pt_BC_PERC/${Year}* ${DirOUT_temp}/Pt_BC_PERC
+    cp -r ${DirIN}/Rainfall_${AccSTR}h/WT/${Year}* ${DirOUT_temp}/WT
+done
---
+++ Scripts/Raw/Retrieve_FC_ecPoint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#########################################################################################################
+# CODE DESCRIPTION
+# Retrieve_FC_ecPoint.sh retrieves ecPoint-Rainfall forecasts from ECFS. Files contain global rainfall forecasts for the considered 
+# accumulation period, ending at the time step indicated in the file name.
+# Runtime: negligible.
+
+# INPUT PARAMETERS DESCRIPTION
+# BaseDateS (date, in YYYYMMDD format): start forecast's basedate to retrieve.
+# BaseDateF (date, in YYYYMMDD format): final forecast's basedate to retrieve.
+# BaseTime (time, in H format, in UTC time): forecast's basetime to retrieve .
+# Acc (number, in H format, in hours): forecasts' accumulation period.
+# CodeVers (string): version of the code that was used to compute the forecasts.
+#                    Available code versions are  listed in the following webpage:
+#                    https://confluence.ecmwf.int/display/EVAL/Forecasts+Code
+# CalVers (string): version of the calibration that was used to compute the forecasts.
+#                   Available calibration versions are listed in the following webpage:
+#                   https://confluence.ecmwf.int/display/EVAL/1.+ecPoint-Rainfall%3A+Developed+or+Under-Development+Calibrations
+# Git_Repo (string): repository's local path.
+# DirIN (string): full path of the ECFS directory containing the forecasts to retrieve.
+# DirOUT (string): relative path containing the retrieved forecasts.
+
+# INPUT PARAMETERS
+BaseDateS=20220101
+BaseDateF=20221231
+BaseTime=0
+Acc=12
+CodeVers="2.0.0"
+CalVers="1.1.0"
+Git_Repo="/ec/vol/ecpoint_dev/mofp/Papers_2_Write/Use_Flash_Floods_Reports_4Verif_USA"
+DirIN="ec:/ecpoint/forecasts/Oper/ECMWF_ENS/Rainfall"
+DirOUT="Data/Raw/FC/ecPoint"
+#########################################################################################################
+
+
+# Setting general variables
+BaseDateS=$(date -d ${BaseDateS} +%Y%m%d)
+BaseDateF=$(date -d ${BaseDateF} +%Y%m%d)
+BaseTimeSTR=$(printf %02d ${BaseTime})
+AccSTR=$(printf %03d ${Acc})
+
+# Setting output directory
+MainDirOUT=${Git_Repo}/${DirOUT}
+mkdir -p ${MainDirOUT}
+
+# Retrieving forecasts from ECFS
+BaseDate=${BaseDateS}
+while [[ ${BaseDate} -le ${BaseDateF} ]]; do
+    echo " "
+    echo "Retrieving forecast for ${BaseDate}"
+    ecfsdir ${DirIN}/${AccSTR}/Code${CodeVers}_Cal${CalVers}/${BaseDate}${BaseTimeSTR} ${MainDirOUT}/${BaseDate}${BaseTimeSTR}
+    BaseDate=$(date -d"${BaseDate} + 1 day" +"%Y%m%d")
+done
