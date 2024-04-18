7bbefa85b9999cda204fb3c5a9df3eff0fe76013
Fatima Pillosu <fatima.pillosu@ecmwf.int>
2024-04-18 10:53:12 +0000

    Updated the location of where the ERA5 ecPoint are retrieved using the definitive one.

--- Scripts/Raw/Retrieve_Analysis_ERA5_ecPoint_tp.sh
+++ Scripts/Raw/Retrieve_Analysis_ERA5_ecPoint_tp.sh
@@ -15,16 +15,18 @@
 # DirOUT (string): relative path containing the retrieved forecasts.
 
 # INPUT PARAMETERS
-YearS=2005
-YearF=2021
+YearS=2022
+YearF=2022
 Acc=12
+ExpVer="0001"
+Code_Cal_Ver="Code2.0.0_Cal1.0.0"
 Git_Repo="/ec/vol/ecpoint_dev/mofp/Papers_2_Write/Use_FlashFloodsRep_4Verif_USA"
-DirIN="/ec/vol/highlander/ERA5_ecPoint_70yr"
+DirIN="/ec/vol/ecpoint/mofp/forecasts/SemiOper/ECMWF_ERA5"
 DirOUT="Data/Raw/Analysis/ERA5_ecPoint/tp"
 ##########################################################################
 
 # Setting general variables
-AccSTR=$(printf %02d ${Acc})
+AccSTR=$(printf %03d ${Acc})
 
 # Setting output directory
 DirOUT_temp=${Git_Repo}/${DirOUT}
@@ -34,6 +36,6 @@ mkdir -p ${DirOUT_temp}/Grid_BC_VALS
 # Retrieving the ERA5_ecPoint data
 for Year in $(seq ${YearS} ${YearF}); do
     echo "Retrieving ERA5_ecPoint for ${Year}"
-    cp -r ${DirIN}/Rainfall_${AccSTR}h/Pt_BC_PERC/${Year}* ${DirOUT_temp}/Pt_BC_PERC
-    cp -r ${DirIN}/Rainfall_${AccSTR}h/Grid_BC_VALS/${Year}* ${DirOUT_temp}/Grid_BC_VALS
+    cp -r ${DirIN}/${ExpVer}/Rainfall/${AccSTR}/${Code_Cal_Ver}/Pt_BC_PERC/${Year}* ${DirOUT_temp}/Pt_BC_PERC
+    cp -r ${DirIN}/${ExpVer}/Rainfall/${AccSTR}/${Code_Cal_Ver}/Grid_BC_VALS/${Year}* ${DirOUT_temp}/Grid_BC_VALS
 done
